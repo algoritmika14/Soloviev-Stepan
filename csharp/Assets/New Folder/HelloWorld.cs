d5d2fe4bb1cab4d0ec5b1414ebca216c76970462
algoritmika14 <algomklass@gmail.com>
2023-03-01 12:49:15 +0500

    sum2

---
+++ csharp/Assets/New Folder/HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("јртЄм крутой"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Fortnite");
+    }
+}
